30e755fed3781cc6a0bb2b11ee8298bf945b01bb
kust1q <dimamishin2133@mail.ru>
2025-09-19 17:06:24 +0000

    small main file fix

--- labs/lab2/var12/main.cpp
+++ labs/lab2/var12/main.cpp
@@ -3,7 +3,7 @@
 int main() {
     std::string input;
     std::cin >> input;
-    money::Money money = money::Money(input);
+    money::Money money(input);
     std::cout << money.CheckBalance() << std::endl;
     return 0;
 }
